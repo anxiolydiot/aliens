e849c89152a21add02f9f7c143fe57ae8b9f5b04
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2016-01-31 02:05:58 -0500

    basics down, messing around with filtering next

---
+++ aliens.js
@@ -0,0 +1,17 @@
+var prompt = require('prompt');
+prompt.start();
+
+prompt.get(['first_name', 'last_name', 'age', 'time_of_siting','sighting_location','other_witness','description_thoughts'], function(err, result) {
+    
+    var report = `FBI aliens sighting report:
+
+    Eye witness name is ${result.first_name} ${result.last_name} at age: ${result.age}
+    According to eye witness sighting happened sometime around ${result.time_of_siting} near ${result.sighting_location}
+    Another possible witness includes a person by the name of ${result.other_witness}
+    The sighting is described as: Some monster thingy and the witness\u0027s initial thoughts are: ${result.description_thoughts}
+    This is the witness\u0027s 11th encounter
+
+    Report conclusion: Complete`;
+
+    console.log(report);
+});
